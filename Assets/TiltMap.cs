5e839f59a50f2da3c8e3435bd6a0aceb3a84afaf
Megan Holmberg <meganoholm@gmail.com>
2016-11-12 14:36:05 -0600

    Initial Commit

---
+++ Assets/TiltMap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltMap : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Vector3 rotationAmount;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody> ();
+		//rotationAmount.Set(0, 0, 0);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		float horizontal = Input.GetAxis ("Horizontal");
+//		float vertical = Input.GetAxis ("Vertical");
+//		//rb.transform.rotation.Set(vertical+100, 0, horizontal+100, 0);
+//		print(rb.transform.rotation);
+
+		rb.transform.Rotate((Vector3.forward*Input.GetAxis("Horizontal")*1)+(Input.GetAxis("Vertical")*Vector3.left*1));
+
+
+	}
+
+
+}
