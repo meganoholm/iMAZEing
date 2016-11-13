9729f3a82af13b12de2ab115ad2a71f563e0fbf5
Megan Holmberg <meganoholm@gmail.com>
2016-11-13 01:57:03 -0600

    quit and play again work now

--- Assets/LoseCollision.cs
+++ Assets/LoseCollision.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LoseCollision : MonoBehaviour {
+	bool collide;
 
 	void OnTriggerEnter(Collider other){
 		Debug.Log ("lose");
+		collide = true;
+	}
+
+	void OnGUI(){
+		if (collide) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 20, 150, 30), "PlayAgain?")) {
+				print ("Starting new game");
+				Scene scene = SceneManager.GetActiveScene();
+				SceneManager.LoadScene (scene.name);
+			}
+			if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 + 20, 150, 30), "Quit?")) {
+				print ("Quit game");
+				Application.Quit ();
+			}
+		}
 	}
 }
