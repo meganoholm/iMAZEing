620a5abacd1bae61e8690fcf8f7b80ce599db2c6
Megan Holmberg <meganoholm@gmail.com>
2016-11-12 21:58:35 -0600

    add more light and fix some things so gravity works better

---
+++ Assets/LoseCollision.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("lose");
+		//other.transform.position = new Vector3(0, 0, 0);
+	}
+}
---
+++ Assets/WinCollision.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCollision : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("win");
+	}
+}
